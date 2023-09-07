60592d74b6c031be75f767c18d57344648898e8b
AnandhSrinivasan <devanandh2902@gmail.com>
2023-09-07 21:50:48 +0530

    Initial Commit

---
+++ Basics_Java/src/Basics_Part1/Print_Statement.java
@@ -0,0 +1,21 @@
+package Basics_Part1;
+
+public class Print_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.print("Hello Milkyway");
+	System.out.print("Hello Universe");
+	System.out.print("Hello Galaxy");
+	System.out.print("Hello World");
+	System.out.print("Hello India");
+	System.out.println("Hello Tamilnadu");
+	System.out.println("Hello Chennai");
+	System.out.println("Hello West KK Nagar");
+	System.out.println("Hello Loganthan Street");
+	System.out.println("Hello 6/11");
+	System.out.println("Hello Muruganandh Srinivasan");
+
+	}
+
+}
